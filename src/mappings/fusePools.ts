c197ee4e9a7aea14161e88d35feaf3ff26b06e0e
platocrat <jack.abe.winfield@gmail.com>
2021-06-25 18:36:33 -0500

    fix createPool()

--- src/mappings/fusePools.ts
+++ src/mappings/fusePools.ts
@@ -4,46 +4,43 @@ import {
   FusePoolDirectory__poolsResult,
   PoolRegistered
 } from '../types/FusePoolDirectory/FusePoolDirectory'
-import { FusePool } from '../types/schema'
-import { Comptroller } from '../types/templates/Comptroller/Comptroller'
+import { FusePool, Comptroller } from '../types/schema'
 
 /**
- * @todo 
- * @dev
+ * @dev Creates FusePool entity
  * @param comptrollerAddress 
  * @returns fusePool A `FusePool` object
  */
-export function createPool(_comptrollerAddress: string, _poolRegistered: PoolRegistered): FusePool {
+export function createPool(_comptrollerAddress: string): FusePool {
   let fusePool: FusePool,
-    fusePoolDirectoryAddress: Address = Address.fromString('0x835482FE0532f169024d5E9410199369aAD5C77E'),
     comptrollerAddress: Address = Address.fromString(_comptrollerAddress)
 
-  let fusePoolDirectory = FusePoolDirectory.bind(fusePoolDirectoryAddress),
-    comptroller = Comptroller.bind(comptrollerAddress)
+  let contract = Comptroller.bind(comptrollerAddress)
+  let admin = contract.try_admin()
 
   fusePool = new FusePool(_comptrollerAddress)
 
-  let pool = fusePoolDirectory.try_pools(_poolRegistered.logIndex),
-    admin = comptroller.try_admin()
-
-  fusePool.id = _comptrollerAddress
-  fusePool.comptroller = comptrollerAddress // pool.value2
   admin.reverted // pool.value1
     ? fusePool.creator = Address.fromString('0x0000000000000000000000000000000000000000')
     : fusePool.creator = admin.value
 
-  if (pool.reverted) {
-    let _timestamp = BigInt.fromString('9999999999'),
-      _blockPosted = BigInt.fromString('9999999999')
-
-    fusePool.name = 'no name detected'
-    fusePool.blockPosted = _blockPosted
-    fusePool.timestampPosted = _timestamp
-  } else {
-    fusePool.name = pool.value.value0
-    fusePool.blockPosted = pool.value.value3 // pool.value3
-    fusePool.timestampPosted = pool.value.value4 // pool.
-  }
+  fusePool.id = contract._address.toHexString()
+  fusePool.comptroller = contract._address
+  fusePool.name = contract._name
+  fusePool.blockPosted = BigInt.fromString('0')
+  fusePool.timestampPosted = BigInt.fromString('0')
 
   return fusePool
+}
+
+export function updateFusePool(
+  fusePoolAddress: Address,
+  blockNumber: i32,
+  blockTimestamp: i32
+): FusePool {
+  let fusePoolID = fusePoolAddress.toHexString()
+  let fusePool = FusePool.load(fusePoolID)
+  if (fusePool == null) {
+    fusePool = createPool(fusePoolID)
+  }
 }
